6e0269f23c5702f3893b976b26991f186c278f07
Priyanshu <hellohyy838@gmail.com>
2023-03-30 23:16:50 +0530

    String modify2
    Input Line

--- Chapter1/2.cpp
+++ Chapter1/2.cpp
@@ -6,7 +6,8 @@ int main()
 {
     string s;
     cout << "Enter the string: ";
-    cin >> s;
+    getline(cin, s); // To take line in input
+    //cin >> s;
     int l1 = s.length();
     cout << "Length of string (s) : " << l1 << endl;
     int l2 = s.size();
