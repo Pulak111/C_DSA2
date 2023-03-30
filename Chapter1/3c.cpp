ad70d47c0a488a7756a88117c2b1146f0a0fccd4
Priyanshu <hellohyy838@gmail.com>
2023-03-30 22:49:51 +0530

    Pass by Refrence

---
+++ Chapter1/3c.cpp
@@ -0,0 +1,20 @@
+// Pass By refrence
+#include<iostream>
+#include<string.h>
+using namespace std;
+void mod_str(string &); // If we want to pass by refrence then we need to declare '&' here.
+int main()
+{
+    string s;
+    cout << "Enter the string: ";
+    cin >> s;
+    mod_str(s);
+    cout << "String inside main function: " << s << endl;
+    return 0;
+}
+void mod_str(string &a)
+{
+    int l = a.length();
+    a[l - 1] = 'A';
+    cout << "String insdid mod_str function : " << a << endl;
+}
