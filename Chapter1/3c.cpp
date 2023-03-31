c9a0b021c9af895ef27d32f0eb8dfa51244a9a68
Pulak111 <104340145+Pulak111@users.noreply.github.com>
2023-04-01 00:41:10 +0530

    Update 3c.cpp

--- Chapter1/3c.cpp
+++ Chapter1/3c.cpp
@@ -7,14 +7,14 @@ int main()
 {
     string s;
     cout << "Enter the string: ";
-    cin >> s;
+    cin >> s; // Hello;
     mod_str(s);
-    cout << "String inside main function: " << s << endl;
+    cout << "String inside main function: " << s << endl; // HellA
     return 0;
 }
 void mod_str(string &a)
 {
     int l = a.length();
     a[l - 1] = 'A';
-    cout << "String insdid mod_str function : " << a << endl;
+    cout << "String insdid mod_str function : " << a << endl; // HellA
 }
