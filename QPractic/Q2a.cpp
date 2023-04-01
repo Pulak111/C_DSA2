58379704bf6c91223870f11833dfd099f6415aa2
Pulak111 <104340145+Pulak111@users.noreply.github.com>
2023-04-02 00:16:22 +0530

    Update Q2a.cpp

--- QPractic/Q2a.cpp
+++ QPractic/Q2a.cpp
@@ -10,6 +10,7 @@ int main()
     cout << "Enter the number :";
     cin >> num;
     vector<int> div;
+    // Time Complexicity O(sqrt n)
     for(int i = 0; i <= sqrt(num); i++) // i*i <= num (Instead of square root)
     {
         if(num % i == 0)
@@ -20,10 +21,12 @@ int main()
                 div.push_back(num2);
         }
     }
+    // Time Complexicity O(n log n) : n is number of factors
     sort(div.begin(), div.end());
     print(div);
     cout << "\nTotal number of divisors : " << div.size();
 }
+// Time Complexicity O(Total Number of divisors)
 void print(vector<int> div)
 {
     for(int i = 0; i < static_cast<int>(div.size()); i++)
