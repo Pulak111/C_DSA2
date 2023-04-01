dbdd6d09425d459663509a53ccce2769180282ce
Pulak111 <104340145+Pulak111@users.noreply.github.com>
2023-04-02 01:15:25 +0530

    Update 2.cpp

--- Chapter1/2.cpp
+++ Chapter1/2.cpp
@@ -9,10 +9,10 @@ int main()
     getline(cin, s); // To take line in input
     //cin >> s;
     int l1 = s.length();
-    cout << "Length of string (s) : " << l1 << endl;
+    cout << "Length of string (" << s << ") using length : " << l1 << endl;
     int l2 = s.size();
-    cout << "Length of string (s) by size : " << l2 << endl;
-    cout << "Letter at 5th index : " << s[5];
+    cout << "Length of string (" << s << ") using size : " << l2 << endl;
+    cout << "Letter at last index : " << s[l1 - 1];
     s[l1 - 1] = 'G';
     cout << "New string : " << s << endl; 
 }
